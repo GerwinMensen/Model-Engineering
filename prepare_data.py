ff249029689a850470bf9c9e7881a8d23f067fe0
GerwinMensen <gerwin.mensen@gmail.com>
2024-02-24 21:03:11 +0100

    Weitere numerische Spalten hinzugefügt, Codeverschlankung

---
+++ prepare_data.py
@@ -0,0 +1,120 @@
+# import python scripts
+import data_load as dl
+
+# import packages
+import numpy as np
+import pandas as pd
+
+
+def prepare_data(dataframe_transactions):
+    # Erste Spalte benennen
+    dataframe_transactions.rename(columns={dataframe_transactions.columns[0]: "ID"}, inplace=True)
+
+    # Extra Spalten für die einzelnen Bestandteile des Timestamps erstellen
+    dataframe_transactions['date'] = dataframe_transactions["tmsp"].dt.date
+    dataframe_transactions['day'] = dataframe_transactions["tmsp"].dt.day
+    dataframe_transactions['time'] = dataframe_transactions["tmsp"].dt.time
+    dataframe_transactions['hour'] = dataframe_transactions["tmsp"].dt.hour
+    dataframe_transactions['minute'] = dataframe_transactions["tmsp"].dt.minute
+    dataframe_transactions['month_as_int'] = dataframe_transactions["tmsp"].dt.month
+    dataframe_transactions['month_as_str'] = dataframe_transactions["tmsp"].dt.month_name()
+    dataframe_transactions['weekday_as_str'] = dataframe_transactions["tmsp"].dt.day_name()
+    dataframe_transactions['weekday_as_int'] = dataframe_transactions["tmsp"].dt.weekday
+    dataframe_transactions['Is_Monday'] = np.where(dataframe_transactions["tmsp"].dt.weekday == 0, 1, 0)
+    dataframe_transactions['Is_Tuesday'] = np.where(dataframe_transactions["tmsp"].dt.weekday == 1, 1, 0)
+    dataframe_transactions['Is_Wednesday'] = np.where(dataframe_transactions["tmsp"].dt.weekday == 2, 1, 0)
+    dataframe_transactions['Is_Thursday'] = np.where(dataframe_transactions["tmsp"].dt.weekday == 3, 1, 0)
+    dataframe_transactions['Is_Friday'] = np.where(dataframe_transactions["tmsp"].dt.weekday == 4, 1, 0)
+    dataframe_transactions['Is_Saturday'] = np.where(dataframe_transactions["tmsp"].dt.weekday == 5, 1, 0)
+    dataframe_transactions['Is_Sunday'] = np.where(dataframe_transactions["tmsp"].dt.weekday == 6, 1, 0)
+
+    # Extra Spalten für den Zahlungsdienstleister erstellen
+    dataframe_transactions['Is_Moneycard'] =  np.where(dataframe_transactions["PSP"] == 'Moneycard', 1, 0)
+    dataframe_transactions['Is_Goldcard'] = np.where(dataframe_transactions["PSP"] == 'Goldcard', 1, 0)
+    dataframe_transactions['Is_UK_Card'] = np.where(dataframe_transactions["PSP"] == 'UK_Card', 1, 0)
+    dataframe_transactions['Is_Simplecard'] = np.where(dataframe_transactions["PSP"] == 'Simplecard', 1, 0)
+
+    # Extra Spalten für die Kartenart erstellen
+    dataframe_transactions['Is_Visa'] = np.where(dataframe_transactions["card"] == 'Visa', 1, 0)
+    dataframe_transactions['Is_Diners'] = np.where(dataframe_transactions["card"] == 'Diners', 1, 0)
+    dataframe_transactions['Is_Master'] = np.where(dataframe_transactions["card"] == 'Master', 1, 0)
+
+    # Extra Spalten für das Land erstellen
+    dataframe_transactions['Is_Germany'] = np.where(dataframe_transactions["country"] == 'Germany', 1, 0)
+    dataframe_transactions['Is_Austria'] = np.where(dataframe_transactions["country"] == 'Austria', 1, 0)
+    dataframe_transactions['Is_Switzerland'] = np.where(dataframe_transactions["country"] == 'Switzerland', 1, 0)
+
+    # temporäre Spalte "tmsp - 1 Minute" erzeugen und anfügen dieser an den Datensatz
+    dataframe_transactions['tmsp_minus_one_min'] = dataframe_transactions["tmsp"] - np.timedelta64(1, 'm')
+
+    # Anzahl Datensätze bestimmen
+    number_of_rows = len(dataframe_transactions)
+    # leeres Series-Objekt erstellen, in welchem abgelegt wird, ob zu dem Datensatz ein vorheriger anderer Datensatz
+    # gefunden werden konnte, der zum gleichen Einkauf gehört
+    has_predecessor = pd.Series([])
+    # leeres Series-Objekt erstellen, in welchem die Höhe der Gebühr abgelegt wird
+    fee = pd.Series([])
+
+    # jeden Datensatz in einer Schleife durchgehen
+    for i in range(0, number_of_rows):
+        # Variablen mit den Werten aus dem aktuell betrachteten Datensatz belegen
+        actual_country = dataframe_transactions.iloc[i].loc['country']
+        actual_amount = dataframe_transactions.iloc[i].loc['amount']
+        actual_date = dataframe_transactions.iloc[i].loc['date']
+        actual_tmsp = dataframe_transactions.iloc[i].loc['tmsp']
+        actual_success = dataframe_transactions.iloc[i].loc['success']
+        actual_psp = dataframe_transactions.iloc[i].loc['PSP']
+        actual_tmsp_minus_one_min = dataframe_transactions.iloc[i].loc['tmsp_minus_one_min']
+
+        # Bestimmen, ob es einen Vorgänger gibt, auf welchen die Kriterien bzgl. desselben Einkaufs zutreffen
+        has_predecessor[i] = np.where(
+                                        # vorheriger Datensatz muss früher sein, aber nicht mehr als 1 Minute früher
+                                        (dataframe_transactions['tmsp'] < actual_tmsp)
+                                        & (dataframe_transactions['tmsp'] > actual_tmsp_minus_one_min)
+                                        # vorheriger Datensatz muss den gleichen Betrag haben
+                                        & (dataframe_transactions['amount'] == actual_amount)
+                                        # vorheriger Datensatz muss aus dem gleichen Land kommen
+                                        & (dataframe_transactions['country'] == actual_country)
+                                        ,1, 0).max()
+
+        # Höhe der Gebühr nach Tabelle in der Aufgabenstellung festlegen
+        if actual_success == 1:
+            if actual_psp == 'Moneycard':
+                fee[i] = 5
+            elif actual_psp == 'Goldcard':
+                fee[i] = 10
+            elif actual_psp == 'UK_Card':
+                fee[i] = 3
+            elif actual_psp == 'Simplecard':
+                fee[i] = 1
+        elif actual_success == 0:
+            if actual_psp == 'Moneycard':
+                fee[i] = 2
+            elif actual_psp == 'Goldcard':
+                fee[i] = 5
+            elif actual_psp == 'UK_Card':
+                fee[i] = 1
+            elif actual_psp == 'Simplecard':
+                fee[i] = 0.5
+
+    # nach Abschluss der Spalte die binäre Variable, ob ein Einkauf Vorgänger hat, anfügen
+    dataframe_transactions.insert(10, 'has_predecessors', has_predecessor)
+    # nach Abschluss der Spalte die Höhe der Gebühr anfügen
+    dataframe_transactions.insert(11, 'fee', fee)
+    # Spalte Timestamp Minus eine Minute wird nicht mehr benötigt, daher löschen
+    dataframe_transactions = dataframe_transactions.drop(labels='tmsp_minus_one_min', axis=1)
+
+    # Tabelle als csv speichern
+    dataframe_transactions.to_csv('Datengrundlage.csv', sep=';')
+    return dataframe_transactions
+
+
+
+
+
+
+
+
+
+
+
